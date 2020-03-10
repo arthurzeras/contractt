dad36f3f67125cfd30c587e03b2b48703ac5bfcf
lucassouto <lucassouto@protonmail.com>
2020-03-10 18:52:13 -0300

    feat(sync): Adiciona sync dos dados do  SurveyMonkey

--- api/survey_monkey/gateway.py
+++ api/survey_monkey/gateway.py
@@ -8,25 +8,40 @@ class SurveyMonkeyAPI:
     TOKEN = settings.SURVEYMONKEY_ACCESS_TOKEN
     HEADERS = {
         'Authorization': f'Bearer {TOKEN}',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
     }
 
     def get_surveys(self):
         url = f'{self.BASE_URL}/surveys/'
 
-        return requests.get(url, headers=self.HEADERS)
+        return requests.get(url, headers=self.HEADERS).json()
 
     def get_survey_by_id(self, id):
         url = f'{self.BASE_URL}/surveys/{id}/'
 
-        return requests.get(url, headers=self.HEADERS)
+        return requests.get(url, headers=self.HEADERS).json()
 
     def get_responses(self, survey_id):
         url = f'{self.BASE_URL}/surveys/{survey_id}/responses/'
 
-        return requests.get(url, headers=self.HEADERS)
+        return requests.get(url, headers=self.HEADERS).json()
 
     def get_response_by_id(self, survey_id, response_id):
-        url = f'{self.BASE_URL}/surveys/{survey_id}/responses/{response_id}'
+        url = f'{self.BASE_URL}/surveys/{survey_id}/responses/{response_id}/details/'  # noqa: E501
 
-        return requests.get(url, headers=self.HEADERS)
+        return requests.get(url, headers=self.HEADERS).json()
+
+    def get_question_email_id(self, survey_id):
+        url_pages = f'{self.BASE_URL}/surveys/{survey_id}/pages/'
+        response_pages = requests.get(url_pages, headers=self.HEADERS)
+        page_id = response_pages.json()['data'][0]['id']
+        url_questions = f'{self.BASE_URL}/surveys/{survey_id}/pages/{page_id}/questions/'  # noqa: E501
+
+        response_questions = requests.get(
+            url_questions,
+            headers=self.HEADERS
+        ).json()
+
+        for question in response_questions['data']:
+            if question['heading'] == 'Digite seu email':
+                return question['id']
