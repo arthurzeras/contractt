1c8103ba48f9c3e81cd6380c2d1b293601f9ef72
Samylla Rodrigues <samylla.rodrigues@instruct.com.br>
2020-03-10 14:24:49 -0300

    Estrutura da fase 02

---
+++ front/src/views/fase-02/fase-02.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Resultado Survey Monkey</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Fase01',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/src/views/fase-02/routes.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    path: '/fase-02',
+    name: 'Fase02',
+    component: () => import(/* webpackChunkName: "about" */ './fase-02.vue')
+  }
+]
