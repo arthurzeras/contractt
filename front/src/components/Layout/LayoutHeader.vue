9e4bebf7332ff136eda4c9ce288800fc6ea92e4d
Arthur Oliveira <arthurolvmorais@gmail.com>
2020-03-10 12:20:48 -0300

    Criação do header e menu lateral

---
+++ front/src/components/Layout/LayoutHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img src="../../assets/images/contractt-min.png" height="70">
+    </a>
+
+    <div class="collapse navbar-collapse">
+      <form class="form-inline ml-auto">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Buscar candidato">
+          <div class="input-group-append">
+            <span class="input-group-text">
+              <i class="fa fa-search"></i>
+            </span>
+          </div>
+        </div>
+      </form>
+    </div>
+  </nav>
+</template>
---
+++ front/src/components/Layout/LayoutSidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="layout-navbar">
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Fase 1</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Fase 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Fase 3</a>
+      </li>
+    </ul>
+  </nav>
+</template>
